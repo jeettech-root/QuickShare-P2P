fabf1bb437ac7f256d6c5c1cd1a65f9061af5f35
Jeet Jansari <jeetjansari22@gmail.com>
2026-02-15 15:51:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,22 @@ function App() {
     const peerConfig = {
         iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
-            { urls: "stun:global.stun.twilio.com:3478" }
+            { urls: "stun:global.stun.twilio.com:3478" },
+            {
+                urls: "turn:openrelay.metered.ca:80",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            },
+            {
+                urls: "turn:openrelay.metered.ca:443",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            },
+            {
+                urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            }
         ]
     };
 
